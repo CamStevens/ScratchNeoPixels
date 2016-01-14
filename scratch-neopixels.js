6ed0f8202e96f53171c293c3023b1dfa66b102d1
CamStevens <cam_stevens@hotmail.com>
2016-01-14 13:55:29 -0800

    Create scratch-neopixels.js

---
+++ scratch-neopixels.js
@@ -0,0 +1,128 @@
+(function(ext) {
+
+  var storedMsg = new Uint8Array(1024);
+
+  var connected = false;
+  var device = null;
+  var poller = null;
+
+  /* TEMPORARY WORKAROUND
+     this is needed since the _deviceRemoved method
+     is not called when serial devices are unplugged*/
+  var sendAttempts = 0;
+
+  var pingCmd = new Uint8Array(1);
+  pingCmd[0] = 1;
+
+  function processInput(data) {
+    for (var i=0; i < data.length; i++) {
+      if (parsingMsg) {
+        if (data[i] == END_MSG) {
+          parsingMsg = false;
+          processMsg();
+        } else {
+          storedMsg[msgBytesRead++] = data[i];
+        }
+      } else {
+        if (data[i] == START_MSG) {
+          parsingMsg = true;
+          msgBytesRead = 0;
+        }
+      }
+    }
+  }
+
+  ext.setPixelColor = function(startPixel, endPixel, red, green, blue) {
+    var output = new Uint8Array(6);
+    output[0] = 0x02;
+    output[1] = startPixel;
+    output[2] = endPixel;
+    output[3] = red;
+    output[4] = green;
+    output[5] = blue;
+    device.send(output.buffer);
+  };
+  
+  ext.startRecording = function() {
+    var output = new Int8Array(1);
+    output[0] = 0x08;
+    device.send(output.buffer);
+  };
+  
+  ext.stopRecording = function() {
+    var output = new Int8Array(1);
+    output[0] = 0x09;
+    device.send(output.buffer);
+  };
+  
+  ext.playbackRecording = function() {
+    var output = new Int8Array(1);
+    output[0] = 0x0A;
+    device.send(output.buffer);
+  };
+  
+  ext._getStatus = function() {
+    if (!connected)
+      return { status:1, msg:'Disconnected' };
+    else
+      return { status:2, msg:'Connected' };
+  };
+
+  ext._deviceRemoved = function(dev) {
+    // Not currently implemented with serial devices
+  };
+
+  var poller = null;
+  ext._deviceConnected = function(dev) {
+    sendAttempts = 0;
+    connected = true;
+    if (device) return;
+    
+    device = dev;
+    device.open({ stopBits: 0, bitRate: 38400, ctsFlowControl: 0 });
+    device.set_receive_handler(function(data) {
+      sendAttempts = 0;
+      var inputData = new Uint8Array(data);
+      processInput(inputData);
+    }); 
+
+    poller = setInterval(function() {
+
+      /* TEMPORARY WORKAROUND
+         Since _deviceRemoved is not
+         called while using serial devices */
+      if (sendAttempts >= 10) {
+        connected = false;
+        device.close();
+        device = null;
+        clearInterval(poller);
+        return;
+      }
+      
+      device.send(pingCmd.buffer); 
+      sendAttempts++;
+
+    }, 1000);
+
+  };
+
+  ext._shutdown = function() {
+    ext.setPixelColor(0, 11, 0, 0, 0);
+    if (device) device.close();
+    if (poller) clearInterval(poller);
+    device = null;
+  };
+
+  var descriptor = {
+    blocks: [
+      [' ', 'set pixels %n to %n to (red:%n, green:%n, blue:%n)', 'setPixelColor', 0, 1, 255, 0, 0],
+      [' ', 'start recording', 'startRecording'],
+      [' ', 'stop recording', 'stopRecording'],
+      [' ', 'playback recording', 'playbackRecording'],
+    ],
+    url: 'http://camstevens.github.io/ScratchNeoPixels'
+  };
+
+  ScratchExtensions.register('ScratchNeoPixels', descriptor, ext, {type:'serial'});
+
+})({});
